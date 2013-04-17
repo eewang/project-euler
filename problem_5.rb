b20070fb605d9c33fedf91f1e0c127029936f2c5
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-17 07:29:04 -0400

    started problem 5

---
+++ problem_5.rb
@@ -0,0 +1,19 @@
+# PROBLEM NUMBER 5
+
+# PROBLEM NAME
+# Smallest Multiple
+
+# PROBLEM DESCRIPTION
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+# PROCESS
+# => OPTION 1
+  # => Go through each number and test if the modulo for each number 1 to 20 is 0. If yes, then that is the answer. Otherwise, move to the next number
+# => OPTION 2
+  # => Find the product of the numbers
+
+# ANSWER
+
+# Products of 
