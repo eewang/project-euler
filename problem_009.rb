697bdbee3e1bcb7a668d3df0e86cf5c5322b790e
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-30 23:41:00 -0400

    problems 9 and 13 in progress

---
+++ problem_009.rb
@@ -0,0 +1,24 @@
+# PROBLEM NUMBER 9
+
+# PROBLEM NAME
+# Special Pythagorean triplet
+
+# PROBLEM DESCRIPTION
+
+# A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+# a2 + b2 = c2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+# PROCESS
+# => Find three numbers for which a^2 + b^2 = c^2 and a + b + c = 1000
+# => Determine a * b * c
+
+# ANSWER
+
+# => 
+
+require 'pry'
+
